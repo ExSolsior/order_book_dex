0959f346cae2d0778b1639cfe0361c51001cd7bd
nomadic4life <orbison.dennis@gmail.com>
2024-10-11 09:13:40 -0700

    did some restructure, moved some instructions into market maker context

--- programs/order_book_dex/src/context/market_maker/mod.rs
+++ programs/order_book_dex/src/context/market_maker/mod.rs
@@ -1,1 +1,8 @@
+pub mod create_order_position_config;
+pub use create_order_position_config::*;
 
+pub mod create_order_position;
+pub use create_order_position::*;
+
+pub mod open_order_position;
+pub use open_order_position::*;
--- programs/order_book_dex/src/context/mod.rs
+++ programs/order_book_dex/src/context/mod.rs
@@ -4,17 +4,8 @@ pub use create_trade_pair::*;
 pub mod create_vault_accounts;
 pub use create_vault_accounts::*;
 
-pub mod create_order_position_config;
-pub use create_order_position_config::*;
-
-pub mod create_order_position;
-pub use create_order_position::*;
-
-pub mod open_order_position;
-pub use open_order_position::*;
-
 pub mod market_taker;
 pub use market_taker::*;
 
-// pub mod market_maker;
-// pub use market_maker::*;
+pub mod market_maker;
+pub use market_maker::*;
