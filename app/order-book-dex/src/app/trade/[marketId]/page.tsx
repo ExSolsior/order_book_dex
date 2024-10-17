81b60c3b78cb67f30a967319b717b1e6a88ce59b
Lezend <31857579+thelezend@users.noreply.github.com>
2024-10-17 21:23:59 +0530

    feat: update page title dynamically based on market selection
    - Added `useEffect` to update the document title with the selected market's token pair and site name.
    - Imported `siteConfig` for site name access.
    - Adjusted import order for better readability.

--- app/order-book-dex/src/app/trade/[marketId]/page.tsx
+++ app/order-book-dex/src/app/trade/[marketId]/page.tsx
@@ -1,15 +1,25 @@
+"use client";
+
 import { Separator } from "@/components/ui/separator";
+import { siteConfig } from "@/config/site";
 import { newMarkets, popular, topGainers } from "@/lib/markets";
+import { useEffect } from "react";
 import { Header } from "./header";
+import MarketDetails from "./market-details";
 import OrderBook from "./order-book";
 import Trade from "./trade";
-import MarketDetails from "./market-details";
 
 export default function Page({ params }: { params: { marketId: string } }) {
   const allMarkets = newMarkets.concat(topGainers, popular);
   const market =
     allMarkets.find((market) => market.marketId === params.marketId) ||
     allMarkets[0];
+
+  useEffect(() => {
+    if (market.marketId) {
+      document.title = `${market.tokenA}/${market.tokenB} - ${siteConfig.name}`;
+    }
+  }, [market]);
 
   return (
     <>
