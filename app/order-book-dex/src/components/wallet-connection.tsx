2f240e484009a665f7df53ff4ff7a6ecbb914e85
Lezend <31857579+thelezend@users.noreply.github.com>
2024-10-18 23:52:23 +0530

    Added a tooltip with a link to the SOON Devnet documentation.

--- app/order-book-dex/src/components/wallet-connection.tsx
+++ app/order-book-dex/src/components/wallet-connection.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { WalletName } from "@solana/wallet-adapter-base";
 import { useWallet } from "@solana/wallet-adapter-react";
+import { HelpCircleIcon } from "lucide-react";
 import { useState } from "react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
@@ -18,7 +20,16 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from "./ui/dropdown-menu";
-import { WalletName } from "@solana/wallet-adapter-base";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "./ui/tooltip";
+import Link from "next/link";
+
+const SOON_DEVNET_DOCS =
+  "https://docs.soo.network/using-soon/getting-started/wallet#backpack-wallet-recommended";
 
 export function WalletConnection() {
   const { select, wallet, wallets, publicKey, disconnect, connecting } =
@@ -42,73 +53,103 @@ export function WalletConnection() {
   };
 
   return (
-    <div className="text-white">
-      <Dialog
-        open={open}
-        onOpenChange={setOpen}
-      >
-        <div className="flex gap-2 items-center">
-          {!publicKey ? (
-            <DialogTrigger asChild>
-              <Button>{connecting ? "Connecting..." : "Connect Wallet"}</Button>
-            </DialogTrigger>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant={"secondary"}>
-                  <Avatar className="w-7 h-7 rounded-full mr-2">
-                    <AvatarImage
-                      src={wallet?.adapter.icon}
-                      alt={wallet?.adapter.name}
-                    />
-                  </Avatar>
-                  <span className="font-mono">
-                    {`${publicKey.toBase58().slice(0, 4)}...${publicKey.toBase58().slice(-3)}`}
-                  </span>
+    <div className="flex items-center">
+      <div className="text-white">
+        <Dialog
+          open={open}
+          onOpenChange={setOpen}
+        >
+          <div className="flex gap-2 items-center">
+            {!publicKey ? (
+              <DialogTrigger asChild>
+                <Button>
+                  {connecting ? "Connecting..." : "Connect Wallet"}
                 </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem>
-                  <Button
-                    variant="ghost"
-                    className="font-semibold"
-                    onClick={handleDisconnect}
-                  >
-                    Disconnect
+              </DialogTrigger>
+            ) : (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant={"secondary"}>
+                    <Avatar className="w-7 h-7 rounded-full mr-2">
+                      <AvatarImage
+                        src={wallet?.adapter.icon}
+                        alt={wallet?.adapter.name}
+                      />
+                    </Avatar>
+                    <span className="font-mono">
+                      {`${publicKey.toBase58().slice(0, 4)}...${publicKey.toBase58().slice(-3)}`}
+                    </span>
                   </Button>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>
+                    <Button
+                      variant="ghost"
+                      className="font-semibold"
+                      onClick={handleDisconnect}
+                    >
+                      Disconnect
+                    </Button>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
 
-          <DialogContent className="w-1/4">
-            <DialogHeader>
-              <DialogTitle className="text-center">Wallets</DialogTitle>
-              <DialogDescription className="text-center">
-                Choose your desired wallet to connect.
-              </DialogDescription>
-            </DialogHeader>
-            <div className="flex flex-col justify-start items-center overflow-auto">
-              {wallets.map((wallet) => (
-                <Button
-                  key={wallet.adapter.name}
-                  onClick={() => handleWalletSelect(wallet.adapter.name)}
-                  variant="ghost"
-                  className="w-full py-6"
-                >
-                  <Avatar className="w-8 h-8 rounded-full mr-2">
-                    <AvatarImage
-                      src={wallet.adapter.icon}
-                      alt={wallet.adapter.name}
-                    />
-                  </Avatar>
-                  <div className="text-lg">{wallet.adapter.name}</div>
-                </Button>
-              ))}
-            </div>
-          </DialogContent>
-        </div>
-      </Dialog>
+            <DialogContent className="w-1/4">
+              <DialogHeader>
+                <DialogTitle className="text-center">Wallets</DialogTitle>
+                <DialogDescription className="text-center">
+                  Choose your desired wallet to connect.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="flex flex-col justify-start items-center overflow-auto">
+                {wallets.map((wallet) => (
+                  <Button
+                    key={wallet.adapter.name}
+                    onClick={() => handleWalletSelect(wallet.adapter.name)}
+                    variant="ghost"
+                    className="w-full py-6"
+                  >
+                    <Avatar className="w-8 h-8 rounded-full mr-2">
+                      <AvatarImage
+                        src={wallet.adapter.icon}
+                        alt={wallet.adapter.name}
+                      />
+                    </Avatar>
+                    <div className="text-lg">{wallet.adapter.name}</div>
+                  </Button>
+                ))}
+              </div>
+            </DialogContent>
+          </div>
+        </Dialog>
+      </div>
+
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="link"
+              onClick={() => window.open(SOON_DEVNET_DOCS, "_blank")}
+            >
+              <HelpCircleIcon className="w-4 h-4 hover:cursor-pointer" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent className="max-w-xs bg-gray-700 text-foreground">
+            <p>
+              Connect to the SOON Devnet by adding a custom RPC in the Backpack
+              wallet.
+            </p>
+            <Link
+              href={SOON_DEVNET_DOCS}
+              target="_blank"
+              className="text-blue-400 hover:text-blue-500"
+            >
+              Learn more
+            </Link>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 }
