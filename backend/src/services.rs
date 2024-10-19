ec62320f12f848388b9b96a5114db60bc229fee3
nomadic4life <orbison.dennis@gmail.com>
2024-10-19 16:56:25 -0700

    I figured out how to deserialize an anchor even coming from the logs. the rest is easy peasy

--- backend/src/services.rs
+++ backend/src/services.rs
@@ -1,6 +1,10 @@
+use std::{borrow::Borrow, ops::DerefMut};
+
 use crate::db::models::{get_market_order_history, get_trade_pair, get_trade_pair_list};
 use actix_web::{get, web, HttpResponse, Responder};
+use base64::engine::{general_purpose, Engine};
 use solana_rpc_client_api::response::{Response, RpcLogsResponse};
+use solana_sdk::{feature_set::leave_nonce_on_success, pubkey::Pubkey};
 
 use crate::AppState;
 use serde::Deserialize;
@@ -70,11 +74,117 @@ pub async fn sanity_check() -> impl Responder {
     HttpResponse::Ok().body("it works")
 }
 
-pub async fn logs_handler(logs_info: Response<RpcLogsResponse>) {
-    // let log = logs_info.value.logs[0].clone();
+pub fn logs_handler(logs_info: Response<RpcLogsResponse>) {
+    println!("------------------------------------------------------------");
+    println!("logs pubsub result: {:?}", logs_info);
 
     let logs = logs_info.value.logs;
 
+    // const PROGRAM_LOG = "Program log: ";
+    // const PROGRAM_DATA = "Program data: ";
+    // const PROGRAM_LOG_START_INDEX = PROGRAM_LOG.length;
+    // const PROGRAM_DATA_START_INDEX = PROGRAM_DATA.length;
+
     // Program data:
-    logs.iter().find(|log| println!("{log}"))
+    match logs.iter().find(|log| log.starts_with("Program data: ")) {
+        Some(data) => decode(data),
+        _ => print!("error"),
+    }
+}
+
+pub fn decode(data: &String) {
+    let decoded = general_purpose::STANDARD.decode(data).unwrap();
+    let discriminator = &decoded[..8];
+
+    if discriminator == [212, 127, 42, 69, 195, 133, 17, 145] {
+        let mut offset = 8;
+
+        let book_config = get_pubkey(&decoded, &mut offset);
+        let token_mint_a = get_pubkey(&decoded, &mut offset);
+        let token_mint_b = get_pubkey(&decoded, &mut offset);
+        let token_program_a = get_pubkey(&decoded, &mut offset);
+        let token_program_b = get_pubkey(&decoded, &mut offset);
+        let sell_market_pointer = get_pubkey(&decoded, &mut offset);
+        let buy_market_pointer = get_pubkey(&decoded, &mut offset);
+
+        let token_symbol_a = get_symbol(&decoded, &mut offset);
+        let token_symbol_b = get_symbol(&decoded, &mut offset);
+
+        let length = 1;
+        let token_decimals_a = u8::from_be_bytes([decoded[offset as usize]]);
+        offset += length;
+
+        let token_decimals_b = u8::from_be_bytes([decoded[offset as usize]]);
+        offset += length;
+
+        let is_reverse = u8::from_be_bytes([decoded[offset as usize]]);
+        offset += length;
+
+        let length = 8;
+        let slot = u64::from_le_bytes(
+            decoded[offset as usize..(offset + length) as usize]
+                .try_into()
+                .expect("expects to be u64"),
+        );
+        offset += length;
+
+        let length = 8;
+        let timestamp = i64::from_le_bytes(
+            decoded[offset as usize..(offset + length) as usize]
+                .try_into()
+                .expect("expects to i64"),
+        );
+
+        println!("{}", book_config);
+        println!("{}", token_mint_a);
+        println!("{}", token_mint_b);
+        println!("{}", token_program_a);
+        println!("{}", token_program_b);
+        println!("{}", sell_market_pointer);
+        println!("{}", buy_market_pointer);
+        println!("{}", sell_market_pointer);
+        println!("{}", buy_market_pointer);
+        println!("{}", token_symbol_a);
+        println!("{}", token_symbol_b);
+        println!("{}", token_decimals_a);
+        println!("{}", token_decimals_b);
+        println!("{}", is_reverse);
+
+        println!("{}", slot);
+        println!("{}", timestamp);
+    }
+
+    println!("{}", data);
+    println!("{:?}, {}", decoded, decoded.len());
+}
+
+pub fn get_pubkey(data: &[u8], offset: &mut u64) -> Pubkey {
+    let start = offset.clone() as usize;
+    let length = 32;
+    let end = start + length as usize;
+
+    *offset += length;
+
+    let pubkey = Pubkey::new_from_array(
+        data[start..end]
+            .try_into()
+            .expect("expect u8 array 32 bytes"),
+    );
+
+    return pubkey;
+}
+
+pub fn get_symbol(data: &[u8], offset: &mut u64) -> String {
+    let start = offset.clone() as usize;
+    let length = 4;
+    let end = start + length as usize;
+
+    let size = u32::from_le_bytes(data[start..end].try_into().expect("slice")) as usize;
+
+    let start = end;
+    let end = start + size;
+
+    *offset += (length + size) as u64;
+
+    return String::from_utf8(data[start..end].to_vec()).unwrap();
 }
