dff61b687e6b86297a6688593a87a1bd5b6590e4
Lezend <31857579+thelezend@users.noreply.github.com>
2024-10-21 00:00:14 +0530

    refactor: update ProgramContext to use null for disconnected state
    - Anchor wallet provider already keeps track of connection state, so we don't need to manage it here.
    - Changed `ProgramContext` default value to `null` instead of an object with `connected: false`.
    - Removed `connected` property from `Value` interface and related logic.

--- app/order-book-dex/src/program/ProgramProvider.tsx
+++ app/order-book-dex/src/program/ProgramProvider.tsx
@@ -34,9 +34,7 @@ type Fill = {
   partial: { partial: { targetPrice: BN } };
 };
 
-export const ProgramContext = createContext<Value | { connected: boolean }>({
-  connected: false
-});
+export const ProgramContext = createContext<Value | null>(null);
 
 export const ProgramProvider = ({ children }: { children: ReactNode }) => {
   // Get provider
@@ -51,13 +49,7 @@ export const ProgramProvider = ({ children }: { children: ReactNode }) => {
 
   if (!program || !userWallet)
     return (
-      <ProgramContext.Provider
-        value={{
-          connected: false
-        }}
-      >
-        {children}
-      </ProgramContext.Provider>
+      <ProgramContext.Provider value={null}>{children}</ProgramContext.Provider>
     );
 
   // Tx: Open Limit Order
@@ -368,7 +360,6 @@ export const ProgramProvider = ({ children }: { children: ReactNode }) => {
   return (
     <ProgramContext.Provider
       value={{
-        connected: userWallet?.publicKey ? true : false,
         createTradePair,
         createMarketOrder,
         fillMarketOrder,
@@ -384,7 +375,6 @@ export const ProgramProvider = ({ children }: { children: ReactNode }) => {
 };
 
 interface Value {
-  connected: boolean;
   createTradePair: (
     tokenMintA: web3.PublicKey,
     tokenMintB: web3.PublicKey,
