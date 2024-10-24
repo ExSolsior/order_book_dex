fa081aad215bab51a85a453c3d20be2d9d4f9171
nomadic4life <orbison.dennis@gmail.com>
2024-10-24 04:07:21 -0700

    sql mock data

---
+++ backend/mock/order_position_config_insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO order_position_config (
+    "pubkey_id",
+    "order_book_config_pubkey",
+    "market_maker_pubkey",
+    "vault_a_pubkey",
+    "vault_b_pubkey",
+    "nonce",
+    "reference"
+) VALUES (
+    '49wBdBzzw9eMre1wT27TD1b8hgW9x9tCWFtvAUmLohtp', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    '3r94vqjXgw8NTuWAf2xXVJZFNF3NMpogHGzukR3UDKGe', 
+    'DMWSPLsKwJrU3a1pNJxWZ3Gb6JykPFSKWtxVR8svhqrB', 
+    'H2NyxBpLBXxjuGMurB9kLJbePaghhEqDenXyptWJaUFe', 
+    0, 
+    0
+),
+
+(
+    '3yJALi5ZMjXnNQvFyPE2sMKjPNYZtZ8jNBxajcGxWp2w', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    'Fg6bTVyTc53uGvUyctgRMaRXuKukM28KvSDXRjfQ3Uru', 
+    'DfBXUGPumZCLMRqAdZbTLND7jB9t2k1cxQbVAh1RMDFz', 
+    '3DYaNhZ11fcZrBXCorqoY51XXHzT7MjWPMmge9ibYsQd', 
+    0, 
+    0
+),
+
+(
+    'C9zVweJ1kCnzVeGpqBVpBSxZYY5Khsec7TqCXNmxcQMs', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    'CPbvhfoLgnUG1uSVBjSYXVeLVqyNyReQoQwnbA6SxotY', 
+    'GFXSVUE696WL6LkiRin5BR6jgpSSh7t85dcHnmVUHqJe', 
+    '9tJJwpjpgZsghxKYUCsxJXySNkBkLQDahrSHA8RTropC', 
+    0, 
+    0
+),
+
+(
+    '2FqYTjMdGDV9mvs4x6F47tJRkLBtnbd7fbcb35kDzXAs', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    '9usz5QxXLMLknDPSMgWbAR7C9boMVmmcUiXB2hkV32wf', 
+    '2dnp89qgZRs4d8WV3nQBWhxuUS13cAoYQatsaU6Sk5mQ', 
+    'ESz3vtYhpRcBTubk7PiFmUqXCqXbTL7SZnP5eZPdump7', 
+    0, 
+    0
+),
+
+(
+    'GSaCLDN6XfmiYQ5Qi8YobZJJpmWn1ah2YJtqQGrLqTuN', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    'UqgxjP8LZtpdQPpdx9nzhMysGRpRsUuCXiwsDDJiEbV', 
+    '7hQQMtS8Az3iGRUWqU25XwnH1N4hMH24zcPMa5itnwy9', 
+    '9HPg2WGyJvp8GJZRQKb2o8fE7hv88CH2up9bbPMCoakc', 
+    0, 
+    0
+),
+
+(
+    'BWwFGMJCCVp8Ru5JQxY4kHd31WUMGtSeMXZYLVtxgcDf', 
+    'BqN7dPo4LheezCRC2kSX5PEyXBRNswvBzLzH7P5w2PWK', 
+    'Day5aAzdktXLdReX7ud7gD5TGX9BtKQhtw6SbzC44Y1N', 
+    'Hnjv6NHPA8A3DmaTrCaASddw6qAjfsYJJMjeJkdDJF8t', 
+    'Dqfmt22vH2v7V4AzibUsZBsxHLaQW62Y6KBgXy8X4qUy', 
+    0, 
+    0
+);
---
+++ backend/mock/order_position_insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO order_position (
+    "pubkey_id",
+    "order_type" ,
+    "price",
+    "size",
+    "is_available",
+    "next_order_position_pubkey",
+    "order_position_config_pubkey",
+    "slot",
+    "timestamp"
+);
